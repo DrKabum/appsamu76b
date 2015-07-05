ce0b765999cd91c71c37bcec05c3c1bd13256e29
Nico Broca <n.broca@gmail.com>
2015-07-05 22:30:22 +0200

    Correction du bug de decompte des problemes a valider

--- src/Samu/GestionVMBundle/Controller/ProblemeVMController.php
+++ src/Samu/GestionVMBundle/Controller/ProblemeVMController.php
@@ -240,11 +240,9 @@ class ProblemeVMController extends Controller
 	public function countNonValideAction()
 	{
 		$count = $this->getDoctrine()->getManager()->getRepository('SamuGestionVMBundle:ProblemeVM')->countAllProblemeNonValide();
-		$decompte = count($count);
 
 		return $this->render('SamuGestionVMBundle:ProblemeVM:new-validation.html.twig', array(
-			'newValidation' => $count,
-			'decompte'      => $decompte
+			'decompte'      => $count
 		));
 	}
 }
