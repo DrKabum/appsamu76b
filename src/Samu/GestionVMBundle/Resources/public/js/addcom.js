7b6e8e2cbe00b881c92db88c4eee4c28f05e1a16
N.Broca <n.broca@gmail.com>
2016-01-27 10:16:08 +0100

    Modifs de version et fix d'une parse error dans problemView

--- src/Samu/GestionVMBundle/Resources/public/js/addcom.js
+++ src/Samu/GestionVMBundle/Resources/public/js/addcom.js
@@ -1,1 +1,14 @@
-$('.new-com').show();
+$('.com-nojs').hide();
+$('.add-com').show();
+
+$('.submit-com').submit(
+	$('.new-com').show();
+	$.ajax({
+		method : this.attr('method'),
+		url    : this.attr('action'),
+		data   : $('this > input[name=com]').val(),
+		succes : function(data) {
+			$('.new-com').hide();
+			
+		}
+	});
