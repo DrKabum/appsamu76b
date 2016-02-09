08c62c523b1caf9323dccb6c50773cf94705b08f
N.Broca <n.broca@gmail.com>
2016-02-09 04:27:37 +0100

    Ajout d'une fonction d'annulation du script de modification d'un commentaire

--- src/Samu/GestionVMBundle/Resources/public/js/editcom.js
+++ src/Samu/GestionVMBundle/Resources/public/js/editcom.js
@@ -1,3 +1,5 @@
+//Script d'apparition de la zone de texte
+
 $(".coms").on("click", "a",function(e)
 {
 	e.preventDefault();
@@ -5,14 +7,27 @@ $(".coms").on("click", "a",function(e)
 
 	if($(this).is(":contains('Modifier')")) 
 	{
-		var content = $("#" + id).html();
-		var action  = $(this).attr('href');
+		if(!$(this).parents(".combox").find("form.modif").length)
+		{
+			//Si le formulaire n'existe pas, je le crée
+			var content = $("#" + id).html();
+			var action  = $(this).attr('href');
 
-		$("#" + id).html('');
-		$("#" + id).append('<form class="modif" id="modif-' + id + '" method="post" action="' + action + '"><input type="text" name="edit-com" id="content"/><input type="submit" value="Modifier le commentaire" /></form>');
-		$("#" + id + " input#content").val(content);
+			$("#" + id).html('');
+			$("#" + id).append('<form class="modif" id="modif-' + id + '" method="post" action="' + action + '"><input type="text" name="edit-com" id="content"/><input type="submit" value="Modifier le commentaire" /></form>');
+			$("#" + id + " input#content").val(content);
+		
+		//Si le formulaire existe déjà, c'est qu'on veut annuler
+		} else {
+			var content = $("#" + id + " input#content").val();
+
+			$("#modif-" + id).remove();
+			$("#" + id).append(content);
+		}
 	}
 });
+
+//Script AJAX de modification du commentaire
 
 $(".coms").on("submit", "form.modif", function(e) { 
 
@@ -34,3 +49,6 @@ $(".coms").on("submit", "form.modif", function(e) {
 		}
 	});
 });
+
+//Script d'annulation du commentaire
+
